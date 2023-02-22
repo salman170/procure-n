8258a859d7cdd790d8c950294d5ca487898eacb5
SALMAN ANSARI <salmanansari02120@gmail.com>
2023-02-22 18:05:40 +0530

    done with some changes in navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="contents">
+      <div className="content" >Leading Procurement</div>
+      <div className="content">Management Expert</div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Navbar.css"
 import logo from "./logo.png"
+import { FaSignInAlt } from "react-icons/fa";
 
 
 const Navbar = () => {
   return (
-    <div className="Navbar">
+    <div className="Navbar" >
       <div className="rightSideContent">
         <div><h2>Home</h2></div>
         <div><h2>About</h2></div>
@@ -20,6 +21,10 @@ const Navbar = () => {
       <div className="leftSideContent">
       <div><h2>Services</h2></div>
       <div><h2>Contact Us</h2></div>
+      <div>
+
+      <FaSignInAlt style={{marginTop:"20px"}} size={35}/>
+      </div>
       </div>
     </div>
   );
